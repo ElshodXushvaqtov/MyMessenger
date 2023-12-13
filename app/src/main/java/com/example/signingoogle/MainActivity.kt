1daac3b2a28eddb9f725e99ad364df08dd9bf12d
Msi <elshodxushvaqtov07@gmail.com>
2023-12-13 10:51:46 +0500

    Done

--- app/src/main/java/com/example/signingoogle/MainActivity.kt
+++ app/src/main/java/com/example/signingoogle/MainActivity.kt
@@ -72,7 +72,7 @@ class MainActivity : ComponentActivity() {
 
                         Button(onClick = {
 
-                            Firebase.auth.signOut()
+                            mGoogleSignInClient.signOut()
 
                         }) {
 
